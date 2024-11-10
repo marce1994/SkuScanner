b8a990dc9c97f15f11df6c1dbb08fb782c5c8a5f
Pablo Bianco <pablo.bianco@digitalvillage.com.au>
2024-11-10 18:41:31 +0000

    deleted amplify

--- amplify/backend.ts
+++
@@ -1,9 +0,0 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { proxy } from './functions/proxy/resource';
-
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
-  proxy
-});
